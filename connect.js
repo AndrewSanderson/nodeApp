0645db18456b0622ec8240c9468112263f2c360a
1306507 <1306507@rgu.ac.uk>
2015-11-23 15:14:46 +0000

    Commit

--- connect.js
+++ connect.js
@@ -7,4 +7,4 @@ var mongodb = require('mongodb');
 var http = require('http');
 //setup our port
 var port = process.env.PORT || 1337;
-
+var url =  'mongodb://AndrewSanderson:1306507@ds057954.mongolab.com:57954/nodeapp1306507';
