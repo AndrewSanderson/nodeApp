26a38ebf2952696418828312b38d6f53db676d2f
1306507 <1306507@rgu.ac.uk>
2015-11-23 15:18:20 +0000

    Commit

--- connect.js
+++ connect.js
@@ -8,3 +8,5 @@ var http = require('http');
 //setup our port
 var port = process.env.PORT || 1337;
 var url =  'mongodb://AndrewSanderson:1306507@ds057954.mongolab.com:57954/nodeapp1306507';
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
