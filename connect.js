6500e9b9316c5a2757eba4c8b057bd25a4bc354b
1306507 <1306507@rgu.ac.uk>
2015-11-23 15:07:46 +0000

    Commit

---
+++ connect.js
@@ -0,0 +1,10 @@
+/**
+ * Created by 1306507 on 23/11/2015.
+ */
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+//and our HTTP server
+var http = require('http');
+//setup our port
+var port = process.env.PORT || 1337;
+
